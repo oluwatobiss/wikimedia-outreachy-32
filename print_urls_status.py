e6386d3ddd0ad799e82dc4dcada1a52275aa81dd
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2026-03-26 09:56:18 +0100

    Move the status and URL printing logic into a function
    The logic for printing URLs and their status codes is now
    encapsulated in a function to enhance code organization,
    readability, and maintainability.
    
    Bug: T418286

--- print_urls_status.py
+++ print_urls_status.py
@@ -12,7 +12,7 @@ def convert_number_to_letter(index):
 
     return final_letter
 
-def create_url_list(file_name, mode):
+def get_urls(file_name, mode):
     with open(file_name, mode) as file:
         urls = []
         for line in file:
@@ -20,19 +20,20 @@ def create_url_list(file_name, mode):
                 urls.append(line.strip())
     return urls
 
-url_list = create_url_list("Task 2 - Intern.csv", "r")
-
-for index, url in enumerate(url_list):
-    label = convert_number_to_letter(index)
-    try:
-        request_object = request.Request(url, method="HEAD")
-        with request.urlopen(request_object) as response:
-            status = response.getcode()
-            text = f"{label}. ({status}) {url}"
+def print_url_and_status(url_list):
+    for index, url in enumerate(url_list):
+        label = convert_number_to_letter(index)
+        try:
+            request_object = request.Request(url, method="HEAD")
+            with request.urlopen(request_object) as response:
+                status = response.getcode()
+                text = f"{label}. ({status}) {url}"
+                print(text)
+        except error.HTTPError as e:
+            text = f"{label}. ({e.code}) {url}"
             print(text)
-    except error.HTTPError as e:
-        text = f"{label}. ({e.code}) {url}"
-        print(text)
-    except error.URLError as e:
-        text = f"{label}. (504) {url}"
-        print(text)
+        except error.URLError as e:
+            text = f"{label}. (504) {url}"
+            print(text)
+
+print_url_and_status(get_urls("Task 2 - Intern.csv", "r"))
